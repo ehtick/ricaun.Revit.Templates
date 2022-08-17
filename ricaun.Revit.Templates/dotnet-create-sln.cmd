b0baeb9a72c42338c09cd35145cc5e84423e7434
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-08-17 10:51:43 -0300

    Clear Build

--- ricaun.Revit.Templates/dotnet-create-sln.cmd
+++ ricaun.Revit.Templates/dotnet-create-sln.cmd
@@ -1,7 +1,6 @@
 @ECHO OFF
-CALL :CreateSolution ricaun-revit-addin-sln ricaun.RevitAddin --Revit2023 True
-CALL :CreateSolution ricaun-revit-addin-23-20-sln ricaun.RevitAddin_23_20
-CALL :CreateSolution ricaun-revit-addin-23-17-sln ricaun.RevitAddin_23_17
+CALL :CreateSolution ricaun-revit-addin-sln ricaun.RevitAddin
+CALL :CreateSolution ricaun-revit-addin-sln ricaun.RevitAddin2023 --Revit2023 True
 timeout 5
 EXIT /B 0
 
@@ -10,6 +9,6 @@ echo CreateSolution - %~1 - %~2
 rmdir /s /q %~2
 mkdir %~2
 cd %~2
-dotnet new %~1 -n %~2 %~3 %~4 %~5 %~6 %~7 %~8
+dotnet new %~1 -n %~2 %~3 %~4 %~5 %~6 %~7 %~8 
 cd ..
 EXIT /B 0
--- ricaun.Revit.Templates/dotnet-help.cmd
+++ ricaun.Revit.Templates/dotnet-help.cmd
@@ -1,5 +1,3 @@
 @ECHO OFF
 dotnet new ricaun-revit-addin-sln --help
-#dotnet new ricaun-revit-addin-23-17-sln --help
-#dotnet new ricaun-revit-addin-23-20-sln --help
 timeout 10
