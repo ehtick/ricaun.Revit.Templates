fa245b76b2b6833ffeb43b5cf1d8ed55cf980b55
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-11-28 12:44:55 -0300

    Update `ricaun.RevitTest.Project` to use multiple target frameworks by default

--- ricaun.Revit.Templates/dotnet-create-project.cmd
+++ ricaun.Revit.Templates/dotnet-create-project.cmd
@@ -2,6 +2,7 @@
 CALL :CreateSolution ricaun-nuke-project ricaun.Build
 CALL :CreateSolution ricaun-revit-addin-project ricaun.RevitAddin.Project
 CALL :CreateSolution ricaun-revittest-project ricaun.RevitTest.Project
+CALL :CreateSolution ricaun-revittest-project ricaun.RevitTest.Project2025 --Revit2025 True
 timeout 5
 EXIT /B 0
 
